d141008046a7f427ab5fdd59ab66e65795a54eb7
Ozhan Esemen <ozhan@esemen.com>
2021-04-07 22:30:33 +0100

    Opening soon page

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Lato', ...defaultTheme.fontFamily.sans],
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
