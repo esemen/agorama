79de4f6a464398aeeef9360960297c33c1f1ec35
Ozhan Esemen <ozhan@esemen.com>
2021-05-07 16:47:39 +0100

    Logotype changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -32,7 +32,7 @@
     class="flex items-center min-h-screen bg-gradient-to-bl from-blue-400 to-green-300 bg-cover bg-center overflow-hidden"
     style="background-image: url('{{ asset('images/bg-landing.jpg') }}')">
     <div class="w-full bg-black bg-opacity-70 p-16 flex flex-col text-white">
-        <h1 class="text-4xl lg:text-6xl xl:text-8xl font-logo font-semibold text-center tracking-wide">Agorama</h1>
+        <h1 class="text-4xl lg:text-6xl xl:text-8xl font-logo font-semibold text-center tracking-wide">agorama</h1>
         <h3 class="text-lg lg:text-2xl xl:text-4xl text-center mt-4 font-light italic">Mediterranean Kitchen</h3>
 
         <hr class="my-8">
