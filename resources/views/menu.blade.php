5e4f676b5c2f912a61ec328e647a4379147abcce
Ozhan Esemen <ozhan@esemen.com>
2021-10-06 21:46:18 +0100

    Menu update

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -16,11 +16,14 @@
                         <div class="flex items-baseline space-x-2">
                             <div class="font-semibold text-gray-700 text-base">{{ $menuItem->name }}
                                 @if($menuItem->vegan ?? false)
-                                    <span class="bg-green-700 font-medium py-1 px-2 text-xs text-white rounded-full">vegan</span>
+                                    <span class="bg-green-700 font-medium ml-2 py-1 px-2 text-xs text-white rounded-full">vegan</span>
+                                @endif
+                                @if($menuItem->hot ?? false)
+                                    <span class="bg-red-600 font-medium ml-2 py-1 px-2 text-xs text-white rounded-full">hot</span>
                                 @endif
                             </div>
                             <div class="flex-grow border-b-2 border-gray-200 border-dotted"></div>
-                            <div class="font-bold text-gold">£{{ number_format($menuItem->price,2) }}</div>
+                            <div class="font-bold text-gold">{{ number_format($menuItem->price,2) }}</div>
                         </div>
                         <div class="flex items-baseline justify-between">
                             <div class="font-normal text-gray-700 text-xs">{{ $menuItem->description }}</div>
