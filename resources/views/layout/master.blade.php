be4460ee28344f5c6f3d3c58d1a3bdc684fad4bb
Ozhan Esemen <ozhan@esemen.com>
2021-10-10 11:34:04 +0100

    Adds order now link

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -16,5 +16,24 @@
 <body class="antialiased">
 @include('includes.header')
 @yield('wrapper')
+<!-- Default Statcounter code for Agorama
+https://www.agorama.co.uk -->
+<script type="text/javascript">
+    var sc_project = 12651202;
+    var sc_invisible = 1;
+    var sc_security = "2c7e9dd8";
+</script>
+<script type="text/javascript"
+        src="https://www.statcounter.com/counter/counter.js"
+        async></script>
+<noscript>
+    <div class="statcounter"><a title="Web Analytics"
+                                href="https://statcounter.com/" target="_blank"><img
+                class="statcounter"
+                src="https://c.statcounter.com/12651202/0/2c7e9dd8/1/"
+                alt="Web Analytics"
+                referrerPolicy="no-referrer-when-downgrade"></a></div>
+</noscript>
+<!-- End of Statcounter Code -->
 </body>
 </html>
